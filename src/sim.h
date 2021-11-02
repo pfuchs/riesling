74ee7ac64f22768ee7c19dffb2726dea7b971edc
Tobias Wood <tobias.wood@kcl.ac.uk>
2021-11-02 17:09:52 +0000

    Implemented parameter generator

--- src/sim.h
+++ src/sim.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "types.h"
+#include "log.h"
 
 namespace Sim {
 
@@ -25,11 +26,53 @@ struct Parameter
   float lo, hi;
   bool logspaced;
 
-  float value(long ii) const;
+  float value(long const ii) const;
 
-  private:
-    float linspace(long i) const;
-    float logspace(long i) const;
+private:
+  float linspace(long const i) const;
+  float logspace(long const i) const;
+};
+
+template<int NP>
+struct ParameterGenerator
+{
+  using Parameters = Eigen::Array<float, NP, 1>;
+
+  ParameterGenerator(std::array<Parameter, NP> const &p) :
+    pars{p}
+  {}
+
+  long totalN() const {
+    long total = 1;
+    for (auto const &par : pars) {
+      total *= par.N;
+    }
+    return total;
+  }
+
+  Parameters values(long const ii) {
+    assert(ii < totalN());
+    Parameters p;
+    long remaining = ii;
+    for (long ip = 0; ip < NP; ip++) {
+      long const parN = remaining % pars[ip].N;
+      remaining /= pars[ip].N;
+      p(ip) = pars[ip].value(parN);
+    }
+    return p;
+  }
+
+  Parameters rand() const {
+    Parameters p;
+    p.setRandom();
+    for (long ip = 0; ip < NP; ip++) {
+      p(ip) = pars[ip].lo + p(ip) * (pars[ip].hi - pars[ip].lo);
+    }
+    return p;
+  }
+
+private:
+  std::array<Parameter, NP> pars;
 };
 
 }
