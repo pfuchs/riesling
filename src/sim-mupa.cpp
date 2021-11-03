593c598a9345800e66ed45c060c105fc4df8b425
Tobias Wood <tobias.wood@kcl.ac.uk>
2021-11-03 13:52:30 +0000

    Parameter generator

--- src/sim-mupa.cpp
+++ src/sim-mupa.cpp
@@ -23,7 +23,8 @@ Result MUPA(
   log.info(FMT_STRING("{} values of T1 from {} to {}s"), T1p.N, T1p.lo, T1p.hi);
   log.info(FMT_STRING("{} values of T2 from {} to {}"), T2p.N, T2p.lo, T2p.hi);
   log.info(FMT_STRING("{} values of B1 from {} to {}"), B1p.N, B1p.lo, B1p.hi);
-  long totalN = T1p.N * T2p.N * B1p.N;
+  ParameterGenerator<3> gen({T1p, T2p, B1p});
+  long totalN = gen.totalN();
   Eigen::MatrixXf sims(totalN, 4 * seq.sps); // SVD expects observations in rows
   Eigen::MatrixXf parameters(totalN, 4);
 
@@ -31,11 +32,10 @@ Result MUPA(
   inv << -1.f, 0.f,
           0.f, 1.f;
   auto task = [&](long const lo, long const hi, long const ti) {
-    long row = lo * T2p.N * B1p.N;
-    for (long it = lo; it < hi; it++) {
-      log.progress(it, lo, hi);
+    for (long ip = lo; ip < hi; ip++) {
+      log.progress(ip, lo, hi);
       // Set up matrices
-      float const T1 = T1p.value(it);
+      float const T1 = T1p.value(ip);
       float const R1 = 1.f / T1;
       Eigen::Matrix2f E1, Eramp, Essi, Ei, Er;
       float const e1 = exp(-R1 * seq.TR);
@@ -51,55 +51,50 @@ Result MUPA(
       Ei << einv, 1 - einv,
             0.f, 1.f;
 
-      for (long iB1 = 0; iB1 < B1p.N; iB1++) {
-        float const B1 = B1p.value(iB1);
-        float const cosa = cos(B1 * seq.alpha * M_PI / 180.f);
-        float const sina = sin(B1 * seq.alpha * M_PI / 180.f);
-        
-        Eigen::Matrix2f A;
-        A << cosa, 0.f,
-             0.f, 1.f;
+      float const B1 = B1p.value(ip);
+      float const cosa = cos(B1 * seq.alpha * M_PI / 180.f);
+      float const sina = sin(B1 * seq.alpha * M_PI / 180.f);
+      
+      Eigen::Matrix2f A;
+      A << cosa, 0.f,
+            0.f, 1.f;
 
-        for (long it2 = 0; it2 < T2p.N; it2++) {
-          float const T2 = T2p.value(it2);
-          float const R2 = 1.f / T2;
-          Eigen::Matrix2f E2;
-          E2 << exp(-R2 * 0.05), 0.f,
-                0.f, 1.f;
+      float const T2 = T2p.value(ip);
+      float const R2 = 1.f / T2;
+      Eigen::Matrix2f E2;
+      E2 << exp(-R2 * 0.05), 0.f,
+            0.f, 1.f;
 
-          // Get steady state after prep-pulse for first segment
-          Eigen::Matrix2f const seg = Essi * Eramp * (E1 * A).pow(seq.sps) * Eramp;
-          Eigen::Matrix2f const SS =
-              Eramp * Essi * E2 * seg * seg * seg * Ei * inv * Essi * seg;
-          float const Mz_ss = SS(0, 1) / (1.f - SS(0, 0));
+      // Get steady state after prep-pulse for first segment
+      Eigen::Matrix2f const seg = Essi * Eramp * (E1 * A).pow(seq.sps) * Eramp;
+      Eigen::Matrix2f const SS =
+          Eramp * Essi * E2 * seg * seg * seg * Ei * inv * Essi * seg;
+      float const Mz_ss = SS(0, 1) / (1.f - SS(0, 0));
 
-          // Now fill in dynamic
-          long col = 0;
-          Eigen::Vector2f Mz{Mz_ss, 1.f};
-          for (long ii = 0; ii < seq.sps; ii++) {
-              sims(row, col++) = Mz(0) * sina;
-              Mz = A * Mz;
-              Mz = E1 * Mz;
-          }
-          Mz = Eramp * Ei * inv * Essi * Eramp * Mz;
-          for (long is = 0; is < 3; is++) {
-            for (long ii = 0; ii < seq.sps; ii++) {
-                sims(row, col++) = Mz(0) * sina;
-                Mz = A * Mz;
-                Mz = E1 * Mz;
-            }
-            Mz = Eramp * Essi * Eramp * Mz;
-          }
-          if (col != (4 * seq.sps)) {
-              Log::Fail("Programmer error");
-          }
-          parameters(row, 0) = Mz_ss;
-          parameters(row, 1) = T1;
-          parameters(row, 2) = T2;
-          parameters(row, 3) = B1;
-          row++;
-        }
+      // Now fill in dynamic
+      long col = 0;
+      Eigen::Vector2f Mz{Mz_ss, 1.f};
+      for (long ii = 0; ii < seq.sps; ii++) {
+          sims(ip, col++) = Mz(0) * sina;
+          Mz = A * Mz;
+          Mz = E1 * Mz;
       }
+      Mz = Eramp * Ei * inv * Essi * Eramp * Mz;
+      for (long is = 0; is < 3; is++) {
+        for (long ii = 0; ii < seq.sps; ii++) {
+            sims(ip, col++) = Mz(0) * sina;
+            Mz = A * Mz;
+            Mz = E1 * Mz;
+        }
+        Mz = Eramp * Essi * Eramp * Mz;
+      }
+      if (col != (4 * seq.sps)) {
+          Log::Fail("Programmer error");
+      }
+      parameters(ip, 0) = Mz_ss;
+      parameters(ip, 1) = T1;
+      parameters(ip, 2) = T2;
+      parameters(ip, 3) = B1;
     }
   };
   auto const start = log.now();
