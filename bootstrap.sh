cdc141e5d4d44de6392620d9b31a6ab65e2c5240
Tobias Wood <tobias@mi3.com>
2021-11-22 11:34:00 +0000

    Attempt M1 toolchain detection

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,12 +7,18 @@
 git submodule update --init --recursive
 
 PLATFORM="$( uname -s )"
+ARCH="$( uname -m )"
 case "$PLATFORM" in
   Linux*)  TOOLCHAIN="../cmake/x64-linux-native.toolchain.cmake";;
-  Darwin*) TOOLCHAIN="../cmake/x64-osx-native.toolchain.cmake";;
+  Darwin*)
+    case "$ARCH" in
+      x86_64*)  TOOLCHAIN="../cmake/x64-osx-native.toolchain.cmake";;
+      arm64*)   TOOLCHAIN="../cmake/vcpkg/triplets/arm64-osx.cmake";;
+    esac
+    ;;
   *)       TOOLCHAIN="../cmake/vcpkg/scripts/buildsystems/vcpkg.cmake";;
 esac
-
+echo $TOOLCHAIN
 # Use Ninja if available, otherwise CMake default
 if [ -x "$( command -v ninja )" ]; then
   GEN="-GNinja"
